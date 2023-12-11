ce0c4267d037f3048b02d64a04acc084336cad1b
Denis Kropp <Leichenbegatter@outlook.com>
2023-12-11 00:53:50 +0000

    improve

--- src/runtime/components/content/TestChat.vue
+++ src/runtime/components/content/TestChat.vue
@@ -40,21 +40,23 @@ function go2() {
 
 <template>
     <div style="display: flex; flex-direction: column;">
-        <div style="width: 40px; height: 2px; background-color: rebeccapurple; margin-top: 1em;" v-if="markdown"></div>
+        <div style="width: 40px; height: 2px; background-color: rebeccapurple; margin-top: 1em;" v-if="markdown" />
 
         <MarkdownStringRenderer :markdownString="markdown" />
 
         <input v-if="!pending" v-model="input" style="color: #222222; width: 100%;" />
 
-                <button v-if="!pending"
-                    style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 2em;"
-                    @click="go1()" :disabled="pending">
-                <div>🏴‍☠️</div>
-            </button>
+        <button v-if="!pending"
+            style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 2em;"
+            @click="go1()" :disabled="pending"
+            >
+            <div>🏴‍☠️</div>
+        </button>
 
-                <button
-                    style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 7em;"
-                    @click="go2()" :disabled="pending">
+        <button
+            style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 7em;"
+            @click="go2()" :disabled="pending"
+            >
             <div v-if="!pending" xstyle="scale: 0.9; margin: -10% -5% -5% -5%; border-top: 1px solid;">
                 <div>✨</div>
                 <slot />
