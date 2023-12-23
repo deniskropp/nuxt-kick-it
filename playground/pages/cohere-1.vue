185ad8f16397f3e16827855d337f5db5e8d09eb1
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-12-23 23:19:00 +0100

    +cohere.ai

---
+++ playground/pages/cohere-1.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { CohereClient } from 'cohere-ai'
+
+const cohere = new CohereClient({
+    token: 'I8aW6BTLwODZgdRTZW3TSAvMA6phiqDujCwKOGj1', // This is your trial API key
+});
+
+(async () => {
+    const response = await cohere.generate({
+        model: 'command',
+        prompt: '{prompt}',
+        maxTokens: 300,
+        temperature: 0.9,
+        k: 0,
+        stopSequences: [],
+        returnLikelihoods: 'NONE'
+    })
+    console.log(`Prediction: ${response.generations[0].text}`)
+})()
+</script>
+
+<template>
+    <cohere-2>
+        
+    </cohere-2>
+</template>
