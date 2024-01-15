3d54e1a5fcfe51601ece4b98906a52dbb770f6b7
Denise Schleier <denise.schleier@outlook.com>
2024-01-14 22:34:50 -0800

    new prompt

--- src/runtime/composables/kickTemplate.ts
+++ src/runtime/composables/kickTemplate.ts
@@ -70,18 +70,16 @@ export function useKickTemplate(init?: Partial<KickTemplate>): KickTemplate {
         messages.unshift(
             {
                 role: 'system',
-                content: `This is a template for document driven content!
+                content: `Generate markdown content according to these rules:
+1. Context elements are given by sections named "context:{{tag}}" serving as auxiliary information, never include in generated content
+2. Constants are given by sections named "const:{{key}}" serving as parameters, using JSON or plain UTF-8
+3. Contents is given by sections named "content" serving as the input data, asking for generated output data
 
 A Section starts with '⫻' on a new line - then '{name}/{type}' - a colon - 'place/index' - and its data...
 1. 'name' being a keyword or token: ['const','content','context']
 2. 'type' being optional information: format, encoding, component type
 3. data as indicated
 4. a few empty lines until the end of the section
-
-Generate responses according to these rules:
-1. Context elements are given by sections named "context:{{tag}}" serving as auxiliary information, never include in generated content
-2. Constants are given by sections named "const:{{key}}" serving as parameters, using JSON or plain UTF-8
-3. Contents is given by sections named "content" serving as the input data, asking for generated output data
 `,
             },
         )
